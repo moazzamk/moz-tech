4572c82abedf2479a9c4601c51ee654556d65a38
Moazzam Khan <moazzamk@gmail.com>
2016-09-27 05:54:19 -0500

    changes for dice

--- crawler/dice.go
+++ crawler/dice.go
@@ -2,16 +2,25 @@ package crawler
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"regexp"
+	"io/ioutil"
 )
 
-type Dice int
-func (i *Dice) Crawl() {
+type Dice struct {
+	Url string
+}
 
-	url := "https://www.dice.com/jobs?q=php"
 
-	req, err := http.NewRequest("POST", url, nil)
+func (i *Dice) getSalaryRange(content string ) []string {
+	return new ([2]string)
+}
+
+func (dice *Dice) Crawl() {
+
+	url := dice.Url
+
+	req, err := http.NewRequest("GET", url, nil)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
@@ -19,14 +28,37 @@ func (i *Dice) Crawl() {
 		fmt.Println("error")
 	}
 
-	defer resp.Body.Close()
+	defer resp.Body.Close();
 
+	//re := regexp.MustCompile("https://www.dice.com/jobs/detail/[^\\\"]+")
+	re := regexp.MustCompile("https://www.dice.com/jobs/[^\\\"]+")
 
 	body, _ := ioutil.ReadAll(resp.Body)
+	matches := re.FindAllString(string(body), 100)
 
-	fmt.Println("response" , string(body))
+
+	for i := 0; i < len(matches); i++ {
+		dice.getDetails(matches[i])
+	}
+
+	//fmt.Println("response" , string(body), matches)
 }
 
 
+func (i *Dice) getDetails(url string) {
+	req, err := http.NewRequest("GET", url, nil)
 
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if (err != nil) {
+		fmt.Println("Error parsing", url)
+	}
+
+	fmt.Println(resp.Body)
+}
+
+func (i *Dice) getJobType(content string) string {
+	
+	return ""
+}
 
--- main.go
+++ main.go
@@ -4,10 +4,23 @@ import (
 	"./crawler"
 )
 
+/*
+
+- find jobs that pay the most
+- find companies that pay the most
+- find technologies that relate to each other
+
+ */
+
 func main() {
 	diceCrawler := new(crawler.Dice)
+	diceCrawler.Url = "https://www.dice.com/jobs?q=php"
 	diceCrawler.Crawl()
 
+
+
+	/*
 	linkedInCrawler := new (crawler.LinkedIn)
 	linkedInCrawler.Crawl()
+	*/
 }
