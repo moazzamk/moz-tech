7698d99dd94420a506f1e39ac26243f07d3e07d3
Moazzam Khan <moazzamk@gmail.com>
2016-09-28 22:24:19 -0500

    Switched to using goquery to parse html on dice. Got job skills working

--- crawler/dice.go
+++ crawler/dice.go
@@ -2,10 +2,10 @@ package crawler
 
 import (
 	"fmt"
-	"net/http"
 	"regexp"
-	"io/ioutil"
 	"strings"
+	"../goquery"
+	"log"
 )
 
 type Dice struct {
@@ -19,16 +19,19 @@ func (i *Dice) getSalaryRange(content string ) []string {
 */
 
 func (dice *Dice) Crawl() {
-	dat, _ := ioutil.ReadFile(`./hourly_pay.html`)
-	content := string(dat)
+	//dat, _ := ioutil.ReadFile(`./hourly_pay.html`)
+	//content := string(dat)
 
-	fmt.Println("skill", dice.getJobSkill(content))
-;
-	text, tel, yo  := dice.getTelecommute(content);
-	fmt.Println(text, tel, yo);
+
+	dice.getDetails(`https://www.dice.com/jobs/detail/Senior-PHP-Developer-RealPage-Inc.-Richardson-TX-75082/10111244/641877?icid=sr1-1p&q=php&l=Dallas,%20TX`)
+
+return;
+/*	fmt.Println("skill", dice.getJobSkill(content))
+
+	tel, yo  := dice.getTelecommuteAndTravel(content);
+	fmt.Println(tel, ":", yo);
 
 	fmt.Println(dice.getPostedDate(content))
-
 
 
 /*
@@ -62,48 +65,34 @@ func (dice *Dice) Crawl() {
 }
 
 
-func (i *Dice) getDetails(url string) {
-	req, err := http.NewRequest("GET", url, nil)
-
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+func (dice *Dice) getDetails(url string) {
+	doc, err := goquery.NewDocument(url)
 	if err != nil {
-		fmt.Println("error")
+		log.Fatal(err)
+		fmt.Println(err, "ERRRR")
 	}
 
-	defer resp.Body.Close();
-
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	fmt.Println("Details for " + url)
-
-	content := string(body)
-	content = strings.Replace(content, "\r", ` `, -1)
-	content = strings.Replace(content, "\n", ` `, -1)
-
-	i.getJobSkill(content)
+	fmt.Println(dice.getJobSkill(doc))
 }
 
-func (dice *Dice) getTelecommute(content string) (string, int, int) {
-	re := regexp.MustCompile(`class="mL20">[^<]+`)
-	text := re.FindString(content)
-	telLower := strings.ToLower(text)
-
-	telecommute := 1
-	if strings.Contains(telLower, `telecommuting not avail`) {
-		telecommute = 0
+func (dice *Dice) getTelecommuteAndTravel(content string) (int, int) {
+	telecommute := 0
+	if !strings.Contains(content, `Telecommuting not available`) {
+		telecommute = 1
 	}
 
 	travel := 0
-	if strings.Contains(telLower, `travel`) && !strings.Contains(telLower, `travel not`) {
+	if strings.Contains(content, `Travel`) && !strings.Contains(content, `Travel not`) {
 		travel = 1
 	}
 
-	return text, telecommute, travel
+	return telecommute, travel
 }
 
 func (dice *Dice) getPostedDate(content string) string {
+
+	// @todo : process jobs posted hours ago not weeks ago
+
 	re := regexp.MustCompile(`Posted Date: </b>[^<]+`)
 	date := re.FindString(content)
 	date = strings.Replace(date, `Posted Date: </b>`, ``, -1)
@@ -112,20 +101,16 @@ func (dice *Dice) getPostedDate(content string) string {
 	return date
 }
 
-func (i *Dice) getJobSkill(content string) []string {
-	re := regexp.MustCompile(`labelskill" title = "[^"]+`)
-
-	match := re.FindString(content)
-	match = strings.Replace(match, `labelskill" title = "`, ``, -1)
-
-	skills := strings.Split(match, `,`)
-
-	return skills
+func (dice *Dice) getJobType(doc *goquery.Document) string {
+	return ``
 }
 
-func (i *Dice) getJobType(content string) string {
+func (dice *Dice) getJobSkill(doc *goquery.Document) []string {
+	var sss string;
 
+	doc.Find(`#labelskill`).Each(func (i int, s *goquery.Selection) {
+		sss = s.Text();
+	})
 
-	return ""
+	return strings.Split(sss, `,`)
 }
-
